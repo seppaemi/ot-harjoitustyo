e10ed211c737e8ef2bf2ca8977113615366b46f3
Seppälä Emilia A <seppaemi@melkki.cs.helsinki.fi>
2022-04-03 16:01:59 +0300

    login colors added

--- src/ui/login.py
+++ src/ui/login.py
@@ -1,67 +1,65 @@
-from tkinter import Tk, ttk, constants,StringVar
-import matplotlib
-matplotlib.use('Agg')
 
-window = Tk()
+from tkinter import font, ttk, StringVar, constants
+from tkinter import Tk
+from turtle import bgcolor
+window=Tk()
+window.title("Login")
 
 class LoginView:
-    def __init__(self, root, handlelogin, handlenewuserview):
-        self._root=root
-        self._handle_login=handlelogin
-        self._handle_new_user_view=handlenewuserview
-        self._frame=None
-        self._username_entry=None
-        self._password_entry=None
-        self._error_variable=None
-        self._error_text=None
+    def __init__(self, root, handle_login=None, handle_show_new_user_view=None):
+        self._root = root
+        self._handle_login = handle_login
+        self._handle_show_new_user_view = handle_show_new_user_view
+        self._frame = None
+        self._username_box = None
+        self._password_box = None
+        self._error_variable = None
+        self._error_text = None
 
         self._initialize()
 
-    def pack(self):
-        self.frame.pack(fill=constants.X)
+    def start(self):
+        self._frame.pack(fill=constants.X)
     
-    def destroy(self):
-        self.frame.destroy()
+    def bgcolor(self):
+        bgcolor(color='pink')
+
+    def remove(self):
+        self._frame.remove()
 
     def _login_handler(self):
-        username=self._username_entry.get()
-        password=self._password_entry.get()
+        username = self._username_box.get()
+        password = self._password_box.get()
 
     def _initialize_username_area(self):
-        username_text=ttk.Label(master=self._frame, text='Username', foreground='white', background='black')
-        self._username_entry=ttk.Entry(master=self._frame)
+        username_text = ttk.Label(master=self._frame, text='Username', foreground= 'black', background='pink')
+        self._username_box = ttk.Entry(master=self._frame)
 
         username_text.grid(padx=10, pady=10, sticky=constants.W)
-        self._username_entry.grid(padx=10, pady=10, sticky=constants.EW)
+        self._username_box.grid(padx=10, pady=10, sticky=constants.EW)
 
     def _initialize_password_area(self):
-        password_text=ttk.Label(master=self, text='Password', foreground='white', background='black')
-        self._password_entry=ttk.Entry(master=self._frame)
+        password_text = ttk.Label(master=self._frame, text='Password', foreground='black', background='pink')
+        self._password_box = ttk.Entry(master=self._frame)
 
         password_text.grid(padx=10, pady=10, sticky=constants.W)
-        self._password_entry.grid(padx=10, pady=10, sticky=constants.EW)
+        self._password_box.grid(padx=10, pady=10, sticky=constants.EW)
 
     def _initialize(self):
-        self._frame=ttk.Frame(master=self._root)
-        self._error_variable=StringVar(self._frame)
-        self._error_text=ttk.Label(master=self._frame, textvariable=self._error_variable, foreground='pink')
-        self._error_label.grid(padx=10, pady=10)
-
+        self._frame = ttk.Frame(master=self._root)
         self._initialize_username_area()
         self._initialize_password_area()
 
-        login_button = ttk.Button(master=self._frame, text='Login', command=self._login_handler)
-        new_user_button=ttk.Button(master=self._frame, text="New user", command=self._handle_new_user_view)
+        login_button = ttk.Button(master=self._frame,text='Login',command=self._login_handler)
+        create_user_button = ttk.Button(master=self._frame,text="Create user",command=self._handle_show_new_user_view,)
 
-        self._frame.grid_columnconfigure(0,weight=1, minsize=500)
-        login_button.grid(padx=10, pady=10, sticky=constants.EW)
-        new_user_button.grid(padx=10, pady=10, sticky=constants.EW)
+        self._frame.grid_columnconfigure(0, weight=1, minsize=500)
 
-        self._hide_error()
+        login_button.grid(padx=5, pady=5, sticky=constants.EW)
+        create_user_button.grid(padx=5, pady=5, sticky=constants.EW)
 
-
-window.title("Login")
 ui = LoginView(window)
+window.configure(bg='pink')
 ui.start()
 
 window.mainloop()
