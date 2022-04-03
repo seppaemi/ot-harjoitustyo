39088be49f1909ffbf024d09efd24ca90bcea874
Seppälä Emilia A <seppaemi@melkki.cs.helsinki.fi>
2022-04-03 14:18:55 +0300

    login view

--- src/ui/login.py
+++ src/ui/login.py
@@ -1,16 +1,20 @@
 from doctest import master
 from tkinter import ttk, StringVar, constants
 
+from numpy import pad
+
 class LoginView:
-    def __init__(self, root, handlelogin, handlecreateuserview):
+    def __init__(self, root, handlelogin, handlenewuserview):
         self._root=root
         self._handle_login=handlelogin
-        self._handle_createuser_view=handlecreateuserview
+        self._handle_new_user_view=handlenewuserview
         self._frame=None
         self._username_entry=None
         self._password_entry=None
+        self._error_variable=None
+        self._error_text=None
 
-        self.initialize()
+        self._initialize()
 
     def pack(self):
         self.frame.pack(fill=constants.X)
@@ -19,8 +23,8 @@ class LoginView:
         self.frame.destroy()
 
     def _login_handler(self):
-        username=self.username_entry.get()
-        password=self.password_entry.get()
+        username=self._username_entry.get()
+        password=self._password_entry.get()
 
     def _initialize_username_area(self):
         username_text=ttk.Label(master=self._frame, text='Username')
@@ -35,3 +39,19 @@ class LoginView:
 
         password_text.grid(padx=10, pady=10, sticky=constants.W)
         self._password_entry.grid(padx=10, pady=10, sticky=constants.EW)
+
+    def _initialize(self):
+        self._frame=ttk.Frame(master=self._root)
+        self._error_variable=StringVar(self._frame)
+        self._error_text=ttk.Label(master=self._frame, textvariable=self._error_variable, foreground='pink')
+        self._error_label.grid(padx=10, pady=10)
+
+        self._initialize_username_area()
+        self._initialize_password_area()
+
+        login_button = ttk.Button(master=self._frame, text='Login', command=self._login_handler)
+        new_user_button=ttk.Button(master=self._frame, text="New user", command=self._handle_new_user_view)
+
+        self._frame.grid_columnconfigure(0,weight=1, minsize=500)
+        login_button.grid(padx=10, pady=10, sticky=constants.EW)
+        new_user_button.grid(padx=10, pady=10, sticky=constants.EW)
