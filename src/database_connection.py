c70e5745fa94ec8ea589203ffa8ca072fed14e69
Seppälä Emilia A <seppaemi@melkki.cs.helsinki.fi>
2022-04-03 20:30:11 +0300

    repositoryt

---
+++ src/database_connection.py
@@ -0,0 +1,11 @@
+import os
+import sqlite3
+
+dirname = os.path.dirname(__file__)
+
+connection = sqlite3.connect('ot-harjoitystyo.db')
+connection.row_factory = sqlite3.Row
+
+
+def get_database_connection():
+    return connection
