21301906225482d9ba8b8a06245032eee4e7d450
Seppälä Emilia A <seppaemi@melkki.cs.helsinki.fi>
2022-04-05 21:41:31 +0300

    viikko3 lopullinen

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,5 +1,5 @@
 import sqlite3
-from entities.users import User
+from entities.user import User
 
 class UserRepository:
     def __init__(self, name):
@@ -8,24 +8,24 @@ class UserRepository:
 
     def create_table(self):
         self.db.execute("begin")
-        self.db.execute("create table users (id integer primary key, name text, password text)")
+        self.db.execute("create table users2 (id integer primary key, name text, password text)")
         self.db.execute("commit")
 
-    def create_user(self, users: User):
+    def create_user(self, users2: User):
         self.db.execute("begin")
-        self.db.execute("insert into users (name, password) values (?,?)", [users.username, users.password])
+        self.db.execute("insert into users2 (name, password) values (?,?)", [users2.username, users2.password])
         self.db.execute("commit")
-        return users
+        return users2
 
     def find_users(self):
         self.db.execute("begin")
-        all_users=self.db.execute("select * from users").fetchall()
+        all_users=self.db.execute("select * from users2").fetchall()
         self.db.execute("commit")
         return all_users
 
     def delete_table(self):
         self.db.execute("begin")
-        self.db.execute("drop table users")
+        self.db.execute("drop table users2")
         self.db.execute("commit")
 
 if __name__ == "__main__":
